43b0538cc5b22a1f5c07ae7653c5403999dfaf75
erikrios <erikriosetiawan49@gmail.com>
2020-03-08 11:08:00 +0700

    Add Constants class

---
+++ app/src/main/java/com/erikriosetiawan/reqresclientandroid/helper/Constants.java
@@ -0,0 +1,7 @@
+package com.erikriosetiawan.reqresclientandroid.helper;
+
+import com.erikriosetiawan.reqresclientandroid.BuildConfig;
+
+public class Constants {
+    public static final String BASE_URL = BuildConfig.BASE_URL;
+}

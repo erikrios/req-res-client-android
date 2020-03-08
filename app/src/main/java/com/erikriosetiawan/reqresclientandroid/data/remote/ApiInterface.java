d9fed7e4190b8d3d911fa99242e0676530d8d3d4
erikrios <erikriosetiawan49@gmail.com>
2020-03-08 14:59:35 +0700

    Update ApiInterface

--- app/src/main/java/com/erikriosetiawan/reqresclientandroid/data/remote/ApiInterface.java
+++ app/src/main/java/com/erikriosetiawan/reqresclientandroid/data/remote/ApiInterface.java
@@ -5,11 +5,12 @@ import com.erikriosetiawan.reqresclientandroid.data.model.BaseResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface ApiInterface {
 
-    @GET("users/2")
-    Call<BaseResponse> getUser();
+    @GET("users/{id}")
+    Call<BaseResponse> getUser(@Path("id") int id);
 
     @GET("users")
     Call<BaseListResponse> getUsers();
